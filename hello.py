568f8ec17438acb7e039fc9fa0790a7c5d62c324
Megha Shyam <shyam2801951@gmail.com>
2014-04-20 00:21:54 +0530

    Akka login page basic

--- hello.py
+++ hello.py
@@ -1,12 +1,23 @@
 import os
 from flask import Flask
+from parse_rest.connection import register
+from flask import request
+from flask import render_template
+
+#register(jNs7bIRWAuvgY3EZMoRkrUyNus3708XOBjGftpzL, NxmOSAToOGSAtnpLzrrsQJT5z83TnsEBULjE0rHP);
 
 app = Flask(__name__)
-app.config.from_pyfile(config.py)
-db = SQLAlchemy(app)
 
-@app.route('/shyam')
-def hello():
-    return '<div style="color:red">Hello World!</div>'
+@app.route('/', methods=['GET'])
+def index():
+	 username = request.cookies.get('username');
+	 isLoggedIn = request.cookies.get('isLoggedIn');
+	 if username and not usename.isspace() and isLoggedIn and not isLoggedIn.isspace():
+	 	return render_template('index.html')
+	 else: 
+		 return render_template('login.html') 
 
+@app.route('/login',methods=['POST'])
+def login():
+	return "200"
 
